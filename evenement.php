4fa193ea9bc3acddd786024e91bb098e6922a4b9
mart1lb² <martinlb28@gmail.com>
2020-02-04 10:48:30 +0100

    include dans la page événement

---
+++ evenement.php
@@ -0,0 +1,5 @@
+<?php include_once 'includes/header.php'; ?> //le fameux include du header pour "include" le header lol
+
+
+
+<?php include_once 'includes/footer.php'; ?> //le fameux footer du header pour "footer" le header lol
