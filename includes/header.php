f0f3ef28d5289bb1bdd5597b58296e2fde886cca
mart1lb² <martinlb28@gmail.com>
2020-02-04 09:49:48 +0100

    maj2

--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/app.css">
 </head>
 <body>
+<header id="navbar" class="sticky-top" >
+    <nav class="navbar bg-transparent navbar-expand-lg navbar-dark" >
+        <a class="navbar-brand" href="#">GameClub</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Le Concept</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Jeux</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
 
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include_once 'includes/header.php'; ?>
 
-HELLO WORLD
+<section class="fixed-top" id="home-jumbotron" style="background-image: url('https://i.pinimg.com/originals/db/c1/1a/dbc11a129f1ff7689e6b55453b57085f.jpg');">
+    <div class="container">
+
+        <h1>Yeeehaaa</h1>
+
+    </div>
+</section>
 
 <?php include_once 'includes/footer.php'; ?>
