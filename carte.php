ee83548e7d4f5e9d131512ca5a20afe7cbc9ff5f
lau44 <lauryn.vandieren@ecoles-wis.net>
2020-02-04 16:11:59 +0100

    M 6

--- carte.php
+++ carte.php
@@ -13,6 +13,52 @@
         </div>
     </section>
 
-    <img src="public/images/carte/terrasse.jpeg" alt="salle bar " title="salle de bar">
+<section>
+    <div="container">
+    <img src="public/images/carte/borabora.png" alt="borabora" title="cocktail borabora">
+    <h3>Borabora</h3>
+    <p>Composition: Jus d'ananas, Jus de fruit de la passion, Jus de citron, Grenadine</p>
+    </div>
+</section>
+
+    <section>
+        <div="container">
+        <img src="public/images/carte/cocktails.png" alt="morijito" title="cocktail morijito">
+        <h3>Morijito</h3>
+        <p>Composition: Rhum, Jus de citron vert, Menthe, Eau gazeuse, Sucre de canne</p>
+        </div>
+    </section>
+
+    <section>
+        <div="container">
+        <img src="public/images/carte/Figdark.png" alt="figdark" title="cocktail figdrak">
+        <h3>Figdrak</h3>
+        <p>Composition: Tequila, Jus d'orange, Sirop de Grenadine</p>
+        </div>
+    </section>
+
+    <section>
+        <div="container">
+        <img src="public/images/carte/Genieen.png" alt="genieen" title="cocktail genieen">
+        <h3>Genieen</h3>
+        <p>Composition: Vodka, Curaçao bleu, Jus de citron</p>
+        </div>
+    </section>
+
+    <section>
+        <div="container">
+        <img src="public/images/carte/Feierabend.png" alt="feierabend" title="cocktail feierabend">
+        <h3>Feierabend</h3>
+        <p>Composition: Rhum brun, Jus de citron, Jus d'orange, Sirop de Grenadine</p>
+        </div>
+    </section>
+
+    <section>
+        <div="container">
+        <img src="public/images/carte/TequilaLime.png" alt="tequilalime" title="cocktail tequilalime">
+        <h3>Tequila Lime</h3>
+        <p>Composition: Limonade, vin mousseux, Sirop d'orange, Sucre...</p>
+        </div>
+    </section>
 
 <?php include_once 'includes/footer.php'; ?>
--- contact.php
+++ contact.php
@@ -1,3 +1,26 @@
 <?php include_once 'includes/header.php'; ?>
 
+    <section id="page-title">
+        <div class="container">
+            <h1>Contact</h1>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <i class="fas fa-phone-volume"></i>
+            <p>Numéro de téléphone</p>
+            <p>09.88.55.67.42</p>
+        </div>
+    </section>
+
+<section>
+    <div class="container">
+       <p> Email</p>
+           <p>Gameclub@gmail.com</p>
+
+    </div>
+</section>
+
+
 <?php include_once 'includes/footer.php'; ?>
