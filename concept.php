7705c2d516a8a15e215783a82310880968925929
mart1lb² <martinlb28@gmail.com>
2020-02-05 12:23:48 +0100

    Auto stash before merge of "master" and "origin/master"

--- concept.php
+++ concept.php
@@ -1,60 +1,22 @@
 <?php include_once 'includes/header.php'; ?>
 
-<<<<<<< HEAD
-<section id="page-title" class="py-5 text-light">
-=======
-
-
-<div id="carouselExampleCaptions" class="carousel slide " data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="public/images/bar/salle%20gaming.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="public/images/bar/bar.jpeg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </div>
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-
-<section id="page-title">
->>>>>>> master
     <div class="container">
         <h1><strong>Le concept</strong></h1>
     </div>
-</section>
 
-<section class="container">
-    <div class="npcard"
+  <div class="container">
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-secondary"> <a href="#sec1">Le Bar</a> </button>
+        <button type="button" href="#sec2" class="btn btn-secondary"><a href="#sec2">Salle détente</a> </button>
+        <button type="button" href="#sec3" class="btn btn-secondary"><a href="#sec3">Salle Gaming</a> </button>
+        <button type="button" href="#sec4" class="btn btn-secondary"><a href="#sec4">Salle Tournament</a> </button>
+    </div>
+  </div>
+
+<section id="sec1" class="container">
+    <div class="npcard "
         <div class="card">
-            <img src="public/images/bar/bar.jpeg" class="card-img-top" alt="salle bar">
+            <img src="public/images/bar/" class="card-img-top" alt="salle bar">
             <div class="card-body ">
                 <h5 class="card-title">Le bar</h5>
                 <p class="card-text">Notre est bar à pour concept un e-sport, c’est à dire un bar comme les autres ou vous pouvez vous
@@ -66,8 +28,7 @@
     </div>
 </section>
 
-
-<section id="jumbotron">
+<section id="sec2">
     <div class="container">
        <div class="card npcard row no-gutters "
             <h2 class="card-title">Le bar</h2>
@@ -80,6 +41,19 @@
     </div>
 </section>
 
+<section >
+<div class="container">
+    <div class="card npcard row no-gutters "
+    <h2 id="sec3" class="card-title">Le bar</h2>
+    <p class="card-text">Notre est bar à pour concept un e-sport, c’est à dire un bar comme les autres ou vous pouvez vous
+        posez, se détendre, boire un verre, être avec ses amis mais aussi s’amuser. Dans une ambiance
+        rétro gaming.</p>
+    <hr class="my-4">
+    <img src="public/images/bar/terrasse.jpeg" alt="salle bar " title="salle de bar">
+</div>
+</section>
+
+<!--
 <section>
     <div class="container">
         <h2>Salle détente</h2>
@@ -117,9 +91,11 @@
 
 
 
+Une deuxième salle pour les geek, nos clients qui aiment joue seul ou avec d’autres personnes pour redécouvrir les joie des jeux rétro de l’ancienne époque. Plongez vous dans un autre univers qui vous changez de votre quotidien.
 
 
 
+Pour les gamers qui aiment jouer en lan, affronter ses adversaires en équipe ou en solo nous avons une salle mis à votre disposition avec une 20 de PC et un écran géant au fond de la salle qui retransmet les scores ou les parties qui sont en train d’être jouer
 
 
 
