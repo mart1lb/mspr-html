0f5d2415490dce1452a044f34d4bd22a1ce5923c
lau44 <lauryn.vandieren@ecoles-wis.net>
2020-02-06 12:17:40 +0100

    jeux

--- jeux.php
+++ jeux.php
@@ -11,10 +11,10 @@
             <h2 class="pt-5">Les consoles</h2>
             <p>Nous avons mis à votre disposition 4 consoles de jeux de différentes époques pour que vous découvriez le
                 plaisir des anciennes consoles de jeux: La première console que nous avons à mis à votre disposition est
-                la Super Nintendo sortie dans les années 90 par les Japonais mais aussi La super NES 2 qui est la
-                version
-                améliorer du design de la Super Nintendo. La deuxième est la playstation de Sony (5 eme génération de
+                la Super Nintendo sortie dans les années 90 par les Japonais, mais aussi La super NES 2 qui est la
+                version améliorer du design de la Super Nintendo. La deuxième est la playstation de Sony (5 eme génération de
                 console) qui est sortie en premier en Chine en 1994.
+                La super Nitendo à sa sortie en Europe en 1992, la console Super Nintendo Entertainment époustoufla les joueurs avec ses graphismes et sa jouabilité qui propulsaient le monde des jeux vidéo dans une ère spectaculairement nouvelle.
                 La Xbox sorite par microsoft en 2001 avec les différents jeux développé par Microsoft game. Pour la
                 dernière console que nous avons dans le bar est l'extraordinaire Magnavox qui est la toute première
                 génération de console apparu en 1972. </p>
@@ -23,8 +23,8 @@
                         <div class="card npcard">
                             <img class="card-img-top" src="public/images/jeux/console.png" alt="Card image cap">
                             <div class="card-body">
-                                <h5 class="card-title">L'Odyssey</h5>
-                                <p class="card-text">console</p>
+                                <h5 class="card-title">Super Nintendo</h5>
+                                <p class="card-text">La Super Nintendo offre un vrai son stéréo, des décors déroulants multiples, et une mémoire intégrée deux fois plus grande que sur son aînée NES - ainsi que des cartouches de taille géante qui permettent au système de recréer d'immenses mondes qu'il faut des semaines pour explorer.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">console</small>
@@ -44,7 +44,7 @@
                             <img class="card-img-top" src="public/images/jeux/premiereconsole.png" alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">Magnavox</h5>
-                                <p class="card-text">console</p>
+                                <p class="card-text">Après 18 mois durant lesquels le design et le nom de la machine évoluèrent, Magnavox annonce la sortie prochaine de l’Odyssey en mai 1972. En septembre de la même année, la première console de salon de l’Histoire est disponible chez les revendeurs Magnavox.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">console</small>
@@ -61,26 +61,28 @@
             <h2 style="color: #23B9D1">Borne d'arcade</h2>
             <p>Voici des bornes d'arcades qui date des années 80-90, venez découvrir l'âge d'or des jeux vidéos qui ont
                 propulsés les jeux vidéos sur le devant de la scène dans les bars, cinéma ect.. Vos parents passer leur
-                temps à jouer aux arcades lors de leur temps libre à votre tour pourquoi pas</p>
+                temps à jouer aux arcades lors de leur temps libre à votre tour pourquoi pas. Une borne d'arcade est un jeu vidéo payant dit « jeu vidéo d'arcade ».
+                On les trouve habituellement dans des lieux ouverts au public comme les bars, les centres commerciaux, certains établissements de divertissement (bowlings, patinoires, cinémas multiplexe, etc.) ainsi que les salles d'arcade. L'origine du nom provient des lieux où ces bornes étaient souvent installées,
+                sous les arcades afin de rentabiliser l'espace. Par extension dans le monde anglophone, une arcade désigne le lieu public en entier, qu'il comporte ou non des arcades.</p>
             <div class="card-deck">
                 <div class="card npcard">
                     <img class="card-img-top" src="public/images/jeux/arcademachine.png" alt="Card image cap">
                     <div class="card-body">
-                        <h5 class="card-title">Genieen</h5>
-                        <p class="card-text">Composition: Vodka, Curaçao bleu, Jus de citron</p>
+                        <h5 class="card-title">Borne d'arcade </h5>
+                        <p class="card-text">Un jeu vidéo d'arcade est un jeu vidéo dans une borne d'arcade. Au début de l'histoire de l'arcade, chaque jeu utilisait un matériel spécifique et unique, avec souvent plusieurs processeurs, des puces sonores et vidéos très spécialisées et toujours la meilleure technologie informatique d'affichage du moment. Les premières bornes n'étaient utilisables qu'avec le jeu avec lequel elles avaient été livrées</p>
                     </div>
                     <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
+                        <small class="text-muted">Borne arcade</small>
                     </div>
                 </div>
                 <div class="card npcard">
                     <img class="card-img-top" src="public/images/jeux/arcade.png" alt="Card image cap">
                     <div class="card-body">
-                        <h5 class="card-title">Tequila Lime</h5>
-                        <p class="card-text">Composition: Limonade, vin mousseux, Sirop d'orange, Sucre</p>
+                        <h5 class="card-title">Borne d'arcade</h5>
+                        <p class="card-text">Jeux King Kong</p>
                     </div>
                     <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
+                        <small class="text-muted">Borne arcade</small>
                     </div>
                 </div>
             </div>
@@ -92,37 +94,42 @@
         <div class="container">
             <h2 style="color: #6F2DBD">Gameboy</h2>
 
-            <p>Pour les fadas d'anciennes gameboy, vous pourrez y jouer sur 4 modèles et d'années différentes</p>
+            <p>Pour les fadas d'anciennes gameboy, vous pourrez y jouer sur 4 modèles et d'années différentes: Elle est la première console portable de la gamme des Game Boy. Descendante
+                de la Game Boy Color (elle-même descendante de la Game Boy), elle offre des performances légèrement supérieures aux consoles de salon 16 bits.
+                Nintendo voulait que sa console ait une apparence originale qui frappe les regards et n'était pas satisfait par les prototypes proposés par les designers traditionnels de l'entreprise. En 2003,
+                Nintendo lance la Game Boy Advance SP, identique techniquement à la Game Boy Advance, mais disposant d'un
+                format plus compact et d'améliorations telles qu'un écran rétro-éclairé, ainsi qu'une batterie lithium-ion rechargeable.
+                Enfin, en 2005, Nintendo sort la Game Boy Micro, dernière déclinaison de sa portable, une console au format « mini » qui propose écran amélioré.</p>
 
             <div class="card-deck">
                 <div class="card npcard">
                     <img class="card-img-top" src="public/images/jeux/console3.png" alt="Card image cap">
                     <div class="card-body">
-                        <h5 class="card-title">Genieen</h5>
-                        <p class="card-text">Composition: Vodka, Curaçao bleu, Jus de citron</p>
+                        <h5 class="card-title">Game Boy Advance</h5>
+                        <p class="card-text">une console de jeux vidéo portable créée par Nintendo en 2001. Elle dispose d'un écran couleur et d'un processeur 32 bits, ce qui en faisait à sa sortie la console portable la plus performante.</p>
                     </div>
                     <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
+                        <small class="text-muted">Gameboy Advance </small>
                     </div>
                 </div>
                 <div class="card npcard">
                     <img class="card-img-top" src="public/images/jeux/game.png" alt="Card image cap">
                     <div class="card-body">
-                        <h5 class="card-title">Feierabend</h5>
-                        <p class="card-text">Composition: Rhum brun, Jus de citron, Jus d'orange, Sirop de Grenadine</p>
+                        <h5 class="card-title">Game Boy</h5>
+                        <p class="card-text">une console portable de jeu vidéo 8-bits de quatrième génération développée et fabriquée par Nintendo.</p>
                     </div>
                     <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
+                        <small class="text-muted">Gameboy</small>
                     </div>
                 </div>
                 <div class="card npcard">
                     <img class="card-img-top" src="public/images/jeux/gameboy.png" alt="Card image cap">
                     <div class="card-body">
-                        <h5 class="card-title">Tequila Lime</h5>
-                        <p class="card-text">Composition: Limonade, vin mousseux, Sirop d'orange, Sucre</p>
+                        <h5 class="card-title">Advance SP</h5>
+                        <p class="card-text"> une console de jeux vidéo portable lancée par Nintendo en mars 2003. Cette console possède les mêmes caractéristiques techniques que la Game Boy Advance mais dispose d'un nouveau design et intègre plusieurs changements.</p>
                     </div>
                     <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
+                        <small class="text-muted">Gameboy</small>
                     </div>
                 </div>
             </div>
