34af59eacd5f736c8292aea93f9ccb72d1994b23
lau44 <lauryn.vandieren@ecoles-wis.net>
2020-02-06 10:45:55 +0100

    noir

--- jeux.php
+++ jeux.php
@@ -7,7 +7,7 @@
     </section>
 
     <section id="game-jumbotron">
-        <div class="container">
+        <div class="container md-5">
             <h2>Les consoles</h2>
             <p>Nous avons mis à votre disposition 4 consoles de jeux de différentes époques pour que vous découvriez le
                 plaisir des anciennes consoles de jeux: La première console que nous avons à mis à votre disposition est
@@ -18,53 +18,74 @@
                 La Xbox sorite par microsoft en 2001 avec les différents jeux développé par Microsoft game. Pour la
                 dernière console que nous avons dans le bar est l'extraordinaire Magnavox qui est la toute première
                 génération de console apparu en 1972. </p>
-            <div class="row">
-                <div class="col">
-                    <div class="card console">
-                        <img src="public/images/jeux/console.png" alt="console" title="console de jeux">
+
+                    <div class="card-deck">
+                        <div class="card npcard">
+                            <img class="card-img-top" src="public/images/jeux/console.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">L'Odyssey</h5>
+                                <p class="card-text">console</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">console</small>
+                            </div>
+                        </div>
+                        <div class="card npcard">
+                            <img class="card-img-top" src="public/images/jeux/console1.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">L'Odyssey</h5>
+                                <p class="card-text">De la première console de salon au casque de réalité virtuelle, plongée au coeur de l'histoire des machines qui nous ont fait jouer, rêver et parfois... enrager</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">console</small>
+                            </div>
+                        </div>
+                        <div class="card npcard">
+                            <img class="card-img-top" src="public/images/jeux/premiereconsole.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Magnavox</h5>
+                                <p class="card-text">console</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">console</small>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card jeux">
-                        <img src="public/images/jeux/console1.png" alt="console sony" title="console de jeux">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <div class="card consolejeux">
-                    <img src="public/images/jeux/consolexbox.png" alt="console xbox" title="console de jeux">
-                </div>
-            </div>
-            <div class="col">
-                <div class="card nintendo">
-                    <img src="public/images/jeux/premiereconsole.png" alt="console nintendo" title="console de jeux">
-                </div>
-            </div>
-        </div>
-        </div>
+
+
     </section>
 
     <section id="borne-jumbotron">
-        <div class="container">
+        <div class="container md-5">
             <h2 style="color: #23B9D1">Borne d'arcade</h2>
             <p>Voici des bornes d'arcades qui date des années 80-90, venez découvrir l'âge d'or des jeux vidéos qui ont
                 propulsés les jeux vidéos sur le devant de la scène dans les bars, cinéma ect.. Vos parents passer leur
                 temps à jouer aux arcades lors de leur temps libre à votre tour pourquoi pas</p>
-            <div class="row">
-                <div class="col">
-                    <div class="card arcade">
-                        <img src="public/images/jeux/arcade.png" alt="arcade 80" title="arcade jeux">
+            <div class="card-deck">
+                <div class="card npcard">
+                    <img class="card-img-top" src="public/images/jeux/arcademachine.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Genieen</h5>
+                        <p class="card-text">Composition: Vodka, Curaçao bleu, Jus de citron</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card machine">
-                        <img src="public/images/jeux/arcademachine.png" alt="arcade 90" title="arcade de jeux">
+                <div class="card npcard">
+                    <img class="card-img-top" src="public/images/jeux/arcade.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Tequila Lime</h5>
+                        <p class="card-text">Composition: Limonade, vin mousseux, Sirop d'orange, Sucre</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
                 </div>
             </div>
         </div>
+
     </section>
 
     <section id="gameboy-jumbotron">
@@ -73,29 +94,40 @@
 
             <p>Pour les fadas d'anciennes gameboy, vous pourrez y jouer sur 4 modèles et d'années différentes</p>
 
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card DS">
-                        <img class="w-100 d-block" src="public/images/jeux/NintendoDS.png" alt="salle gaming " title="DS">
+            <div class="card-deck">
+                <div class="card npcard">
+                    <img class="card-img-top" src="public/images/jeux/console3.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Genieen</h5>
+                        <p class="card-text">Composition: Vodka, Curaçao bleu, Jus de citron</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="card gameb">
-                        <img class="w-100 d-block" src="public/images/jeux/game.png" alt="salle gaming " title="game">
+                <div class="card npcard">
+                    <img class="card-img-top" src="public/images/jeux/game.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Feierabend</h5>
+                        <p class="card-text">Composition: Rhum brun, Jus de citron, Jus d'orange, Sirop de Grenadine</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="card gamea">
-                        <img class="w-100 d-block" src="public/images/jeux/gameboy.png" alt="salle gaming " title="gameboy">
+                <div class="card npcard">
+                    <img class="card-img-top" src="public/images/jeux/gameboy.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Tequila Lime</h5>
+                        <p class="card-text">Composition: Limonade, vin mousseux, Sirop d'orange, Sucre</p>
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card gameboi">
-                        <img class="w-100 d-block" src="public/images/jeux/console3.png" alt="salle gaming " title="consolelittle">
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
                 </div>
             </div>
         </div>
+
     </section>
 
 
