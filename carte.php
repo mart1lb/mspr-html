d8f3c7c71d8f889cd23e4ef2f6ef15b78eb420d1
lau44 <lauryn.vandieren@ecoles-wis.net>
2020-02-07 09:11:06 +0100

    update: fix update de la mort qui tue

--- carte.php
+++ carte.php
@@ -13,81 +13,82 @@
         </div>
     </section>
 
-<section>
-<div class="container mb-5">
-    <div class="card-deck">
-        <div class="card scard">
-            <img class="card-img-top" src="public/images/carte/borabora.png" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Bora-bora</h5>
-                <p class="card-text">Composition: Jus d'ananas, Jus de fruit de la passion, Jus de citron, Grenadine</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Your drink in 3 min</small>
+    <section>
+        <div class="container mb-5">
+            <div class="card-deck">
+                <div class="card scard">
+                    <img class="card-img-top" src="public/images/carte/borabora.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Bora-bora</h5>
+                        <p class="card-text">Composition: Jus d'ananas, Jus de fruit de la passion, Jus de citron,
+                            Grenadine</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Your drink in 3 min</small>
+                    </div>
+                </div>
+                <div class="card scard">
+                    <img class="card-img-top" src="public/images/carte/cocktails.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Mojito</h5>
+                        <p class="card-text">Composition: Rhum, Jus de citron vert, Menthe, Eau gazeuse, Sucre de
+                            canne</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Your drink in 3 min</small>
+                    </div>
+                </div>
+                <div class="card scard">
+                    <img class="card-img-top" src="public/images/carte/Figdark.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Figdrak</h5>
+                        <p class="card-text">Composition: Tequila, Jus d'orange, Sirop de Grenadine</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Your drink in 3 min</small>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="card scard">
-            <img class="card-img-top" src="public/images/carte/cocktails.png" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Mojito</h5>
-                <p class="card-text">Composition: Rhum, Jus de citron vert, Menthe, Eau gazeuse, Sucre de canne</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Your drink in 3 min</small>
-            </div>
-        </div>
-        <div class="card scard">
-            <img class="card-img-top" src="public/images/carte/Figdark.png" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Figdrak</h5>
-                <p class="card-text">Composition: Tequila, Jus d'orange, Sirop de Grenadine</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Your drink in 3 min</small>
-            </div>
-        </div>
-    </div>
-</div>
-</section>
+    </section>
 
 
-<section id="card">
-    <div class="container mb-5">
-        <div class="card-deck">
-            <div class="card scard">
-                <img class="card-img-top" src="public/images/carte/Genieen.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Genieen</h5>
-                    <p class="card-text">Composition: Vodka, Curaçao bleu, Jus de citron</p>
+    <section id="card">
+        <div class="container mb-5">
+            <div class="card-deck">
+                <div class="card scard">
+                    <img class="card-img-top" src="public/images/carte/Genieen.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Genieen</h5>
+                        <p class="card-text">Composition: Vodka, Curaçao bleu, Jus de citron</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Your drink in 3 min</small>
+                    </div>
                 </div>
-                <div class="card-footer">
-                    <small class="text-muted">Your drink in 3 min</small>
+                <div class="card scard">
+                    <img class="card-img-top" src="public/images/carte/Feierabend.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Feierabend</h5>
+                        <p class="card-text">Composition: Rhum brun, Jus de citron, Jus d'orange, Sirop de Grenadine</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Your drink in 3 min</small>
+                    </div>
                 </div>
-            </div>
-            <div class="card scard">
-                <img class="card-img-top" src="public/images/carte/Feierabend.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Feierabend</h5>
-                    <p class="card-text">Composition: Rhum brun, Jus de citron, Jus d'orange, Sirop de Grenadine</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Your drink in 3 min</small>
-                </div>
-            </div>
-            <div class="card scard">
-                <img class="card-img-top" src="public/images/carte/Tequilalime.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Tequila Lime</h5>
-                    <p class="card-text">Composition: Limonade, vin mousseux, Sirop d'orange, Sucre</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Your drink in 3 min</small>
+                <div class="card scard">
+                    <img class="card-img-top" src="public/images/carte/Tequilalime.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Tequila Lime</h5>
+                        <p class="card-text">Composition: Limonade, vin mousseux, Sirop d'orange, Sucre</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Your drink in 3 min</small>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-
+    </section>
 
 
 <?php include_once 'includes/footer.php'; ?>
--- evenement.php
+++ evenement.php
@@ -3,98 +3,69 @@
 
     <section class="pt-5">
         <div class="container pt-5">
-            <div class="jumbotron p-3 p-md-5 text-white rounded" id="conceptheader" style="background-image: url(public/images/homepage/tournoisbanner.gif);)">
+            <div class="jumbotron p-3 p-md-5 text-white rounded" id="conceptheader"
+                 style="background-image: url(public/images/homepage/tournoisbanner.gif);)">
                 <div class="col-md-6 px-0">
                     <div class="card bg-transparent rounded" style="backdrop-filter: blur(2px)">
                         <h1 class="display-4 font-italic">Liste des Events</h1>
-                        <p class="lead my-3 text-white">Prenez part aux tournois et venez affronter vos amis sur de nombreux jeux, toutes platformes confondu</p>
+                        <p class="lead my-3 text-white">Prenez part aux tournois et venez affronter vos amis sur de
+                            nombreux jeux, toutes platformes confondu</p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 
-<section>
-<<<<<<< Updated upstream
-    <div class="container" id="date">
-=======
-<<<<<<< Updated upstream
-    <div class="container ">
-=======
-<<<<<<< HEAD
-    <div class="container mt-5 ">
-=======
-    <div class="container" id="date">
->>>>>>> master
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-        <div class="card-deck mb-3 text-center">
-            <div class="card  npcard mb-4 box-shadow">
-                <div class="card-header">
-                    <h2 class="my-0 font-weight-normal">Leagues of Legends Tournament</h2>
-                </div>
-                <p><strong>20 Juin</strong></p>
-                <div class="card-body">
-                    <h3 class="card-title pricing-card-title">5€ <small class="text-muted">/ Pers</small></h3>
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li>Première conso offerte</li>
-                        <li>Lot à Gagner pour le top 3</li>
-                        <li>Ambiance garantie</li>
-                    </ul>
-                    <button type="button" class="btn btn-lg btn-block btn-outline-primary">S'inscrire</button>
-                </div>
-            </div>
-</section>
-
     <section>
-<<<<<<< Updated upstream
         <div class="container" id="date">
-=======
-<<<<<<< Updated upstream
-        <div class="container ">
-=======
-<<<<<<< HEAD
-        <div class="container mt-5 ">
-=======
-        <div class="container" id="date">
->>>>>>> master
->>>>>>> Stashed changes
->>>>>>> Stashed changes
             <div class="card-deck mb-3 text-center">
                 <div class="card  npcard mb-4 box-shadow">
                     <div class="card-header">
-                        <h2 class="my-0 font-weight-normal">Fortnite</h2>
+                        <h2 class="my-0 font-weight-normal">Leagues of Legends Tournament</h2>
                     </div>
-                    <p><strong>10 Juillet</strong></p>
+                    <p><strong>20 Juin</strong></p>
                     <div class="card-body">
-                        <h3 class="card-title pricing-card-title">10€ <small class="text-muted">/ Pers</small></h3>
+                        <h3 class="card-title pricing-card-title">5€ <small class="text-muted">/ Pers</small></h3>
                         <ul class="list-unstyled mt-3 mb-4">
                             <li>Première conso offerte</li>
-                            <li>Réduction sur tout les snacks</li>
                             <li>Lot à Gagner pour le top 3</li>
                             <li>Ambiance garantie</li>
                         </ul>
                         <button type="button" class="btn btn-lg btn-block btn-outline-primary">S'inscrire</button>
                     </div>
                 </div>
+            </div>
+        </div>
     </section>
 
-<<<<<<< Updated upstream
-    <section id="card">
-        <div class="container" id="date">
-=======
-<<<<<<< HEAD
     <section>
-<<<<<<< Updated upstream
-        <div class="container ">
-=======
-        <div class="container mt-5">
-=======
-    <section id="card">
         <div class="container" id="date">
->>>>>>> master
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+            <div class="card-deck mb-3 text-center">
+                <div class="card npcard mb-4 box-shadow">
+                    <div class="card-header">
+                        <h2 class="my-0 font-weight-normal">Fortnite</h2>
+                    </div>
+                    <p><strong>10 Juillet</strong></p>
+                    <div class="card-body">
+                        <h3 class="card-title pricing-card-title">10€ <small class="text-muted">/
+                                Pers</small></h3>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>Première conso offerte</li>
+                            <li>Réduction sur tout les snacks</li>
+                            <li>Lot à Gagner pour le top 3</li>
+                            <li>Ambiance garantie</li>
+                        </ul>
+                        <button type="button" class="btn btn-lg btn-block btn-outline-primary">
+                            S'inscrire
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="card">
+        <div class="container">
             <div class="card-deck mb-3 text-center">
                 <div class="card  npcard mb-4 box-shadow">
                     <div class="card-header">
@@ -102,16 +73,21 @@
                     </div>
                     <p><strong>27 Aout</strong></p>
                     <div class="card-body">
-                        <h3 class="card-title pricing-card-title">5€ <small class="text-muted">/ Pers</small></h3>
+                        <h3 class="card-title pricing-card-title">5€ <small class="text-muted">/
+                                Pers</small></h3>
                         <ul class="list-unstyled mt-3 mb-4">
                             <li>Première conso offerte</li>
                             <li>Réduction sur tout les snacks</li>
                             <li>Lot à Gagner pour le top 10</li>
                             <li>Ambiance garantie</li>
                         </ul>
-                        <button type="button" class="btn btn-lg btn-block btn-outline-primary">S'inscrire</button>
+                        <button type="button" class="btn btn-lg btn-block btn-outline-primary">
+                            S'inscrire
+                        </button>
                     </div>
                 </div>
+            </div>
+        </div>
     </section>
 
 <?php include_once 'includes/footer.php'; ?>
